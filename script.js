0cf464c5dc90ac4eabfc319e0a6c23a0e6029a2a
Rizkyalfasha <adityakrui518@gmail.com>
2025-09-16 20:21:21 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+function showHome() {
+  document.getElementById("content").innerHTML = `
+    <h2>SELAMAT DATANG DI WEBSITE NYA RIZKY ALFASHA</h2>
+    <p>
+      <a href="https://www.tiktok.com/@rizkyalfasha369" target="_blank">
+        Klik di sini untuk follow @rizkyalfasha369
+      </a>
+    </p>
+  `;
+}
+
+function showInfo() {
+  document.getElementById("content").innerHTML = `
+    <h2>Informasi Akun</h2>
+    <p><b>Nama:</b> Rizky</p>
+    <p><b>Kelas:</b> 7</p>
+    <p><b>Alamat:</b> Way Napal</p>
+  `;
+}
